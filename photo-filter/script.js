320a4818cdf26985fa625f25f6c2c4dacf3be598
barmenski <1kiloom@gmail.com>
2021-04-25 02:14:37 +0300

    feat: next(with time) - work

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -83,8 +83,25 @@ MAIN.addEventListener("input", startCorrespondOver);
 MAIN.addEventListener("mouseup", stopCorrespondOver);
 EDITOR.addEventListener("click", startEditor);
 
+var base="";
+const baseNight = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/night/';
+const baseMorning = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/morning/';
+const baseDay = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/day/';
+const baseEvening = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/evening/';
 
-const base = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/night/';
+let now = new Date();
+let hours=now.getHours();
+console.log(hours);
+if ((hours>6)&&(hours<12)) {
+    base=baseMorning;
+} else if ((hours>=12)&&(hours<18)) {
+        base=baseDay;
+    } else if ((hours>=18)&&(hours<00)) {
+        base=baseEvening;
+        } else {
+            base=baseNight;
+        }
+
 const images = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
 let i = 0;
 const body = document.querySelector('img');
@@ -94,8 +111,7 @@ function viewBgImage(src) {
   const img = new Image();
   img.src = src;
   img.onload = () => {      
-    //body.style.backgroundImage = `url(${src})`;
-body.setAttribute("src", `${src}`);
+  body.setAttribute("src", `${src}`);
   }; 
 }
 
