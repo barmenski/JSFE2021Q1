2ba0144f76e0baafa37fd8dea3caf42ba9e55849
barmenski <1kiloom@gmail.com>
2021-04-26 00:37:27 +0300

    feat: canvas - work

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -79,93 +79,96 @@ const startEditor = (event) => {
     };
 }
 
-
 EDITOR.addEventListener("click", startEditor);
 /*----------------------------------For reset end--------------------------------*/
 
 
 /*----------------------------------For next picture start--------------------------------*/
-var img1 = new Image();
-img1.src = "assets/img/img.jpg";
-img1.setAttribute('crossOrigin', 'anonymous'); 
-var img2 = new Image();
-img2.src = "assets/img/img.jpg";
-img2.setAttribute('crossOrigin', 'anonymous');
-const canvas = document.querySelector('canvas');
-//const body = document.querySelector('img');
-const imageContainer = document.querySelector('img');
-const btnNext = document.querySelector('.btn-next'); //выбрали кнопку для NEXT PICTURE
-const fileInput = document.querySelector('input[type="file"]'); //выбрали кнопку для LOAD PICTURE
-
-/*----исходные данные для картинок----*/
-
-var base="";
+var pictureSrc="assets/img/img.jpg";
+var base = '';
 const baseNight = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/night/';
 const baseMorning = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/morning/';
 const baseDay = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/day/';
 const baseEvening = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/evening/';
-
-
-
 const images = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
 let i = 0;
+const imageContainer = document.querySelector('img');
+const btnNext = document.querySelector('.btn-next');
 
-
-function viewBgImage(src) { 
-    img1.src = src;
-    img1.onload = function() {
-        canvas.width = img1.width;
-        canvas.height = img1.height;
-        const ctx = canvas.getContext("2d");
-        ctx.drawImage(img1, 0, 0);// рисуем canvas
-        imageContainer.setAttribute("src", `${img1.src}`);// назначаем src для img
+function viewBgImage(src) {  
+  const img = new Image();
+    img.src = src;
+    pictureSrc = src;
+    drawPic(pictureSrc);
+    console.log("pictureSrc of NEXT: ", pictureSrc);
+    img.onload = () => {      
+    imageContainer.setAttribute("src", `${src}`);
   }; 
 }
 
 function getImage() {
     let now = new Date();
     let hours=now.getHours();
-    if ((hours>6)&&(hours<12)) {
-        base = baseMorning;
-    } else if ((hours>=12)&&(hours<18)) {
-        base = baseDay;
-    } else if ((hours>=18)&&(hours<00)) {
-        base = baseEvening;
-    } else {
-            base = baseNight;
-    }
+        if ((hours>6)&&(hours<12)) {
+            base = baseMorning;
+        } else if ((hours>=12)&&(hours<18)) {
+            base = baseDay;
+        } else if ((hours>=18)&&(hours<00)) {
+            base = baseEvening;
+        } else {
+                base = baseNight;
+        }
     const index = i % images.length;
     const imageSrc = base + images[index];
-    viewBgImage(imageSrc); //вызываем viewBgImage с подготовленным src
+    viewBgImage(imageSrc);
     i++;
     btnNext.disabled = true;
     setTimeout(function() { btnNext.disabled = false }, 1000);
-} 
+}
 
-btnNext.addEventListener('click', getImage);//обработчик нажатия на кнопку NEXT PICTURE
-
-
+btnNext.addEventListener('click', getImage);
 /*----------------------------------For next picture end--------------------------------*/
 
+/*----------------------------------For upload picture end--------------------------------*/
 
-/*----------------------------------For upload picture start--------------------------------*/
+const btnLoad = document.querySelector('input[type="file"]');
 
-/*
-const fileInput = document.querySelector('input[type="file"]');
-const imageContainer = document.querySelector('img');
-
-fileInput.addEventListener('change', function(e) {
-  const file = fileInput.files[0];
+function imgLoad() {
+  const file = btnLoad.files[0];
   const reader = new FileReader();
   reader.onload = () => {
     const img = new Image();
     img.src = reader.result;
+    pictureSrc = reader.result;
+    drawPic(pictureSrc);
+    console.log("pictureSrc of LOAD: ", pictureSrc);
     imageContainer.setAttribute("src", `${img.src}`);
   }
   reader.readAsDataURL(file);
-});
-*/
-/*----------------------------------For upload picture end--------------------------------*/
+};
+
+btnLoad.addEventListener('change', imgLoad);
+
+/*----------------------------------For make canvas start--------------------------------*/
+
+const canvas = document.querySelector('canvas');
+
+  function drawPic(src) {
+    const img = new Image();
+    img.setAttribute('crossOrigin', 'anonymous');
+    img.setAttribute("src", `${src}`); 
+    console.log("img.src of CANVAS: ", img.src);
+    img.onload = function() {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      const ctx = canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0);
+    };  
+  }
+  drawPic(pictureSrc);
+/*----------------------------------For make canvas end--------------------------------*/
+
+
 
 /*----------------------------------For save picture start--------------------------------*/
 
@@ -173,44 +176,6 @@ fileInput.addEventListener('change', function(e) {
 
 
 
-//var img = new Image();
-
-
-
-function loadImg() {
-  const file = fileInput.files[0];
-  const reader = new FileReader();
-  //img2.src = reader.result;
-  reader.onload = () => {
-      
-        img2.src = reader.result;
-        canvas.width = img2.width;
-        canvas.height = img2.height;
-        const ctx = canvas.getContext("2d");
-        ctx.drawImage(img2, 0, 0);
-        imageContainer.setAttribute("src", `${reader.result}`);
-      
-  }
-  reader.readAsDataURL(file);
-};
-
-
-
-
- const img = new Image();
- img.src = "assets/img/img.jpg";
-  function drawCanvas(){
-    img.onload = function() {
-        canvas.width = img.width;
-        canvas.height = img.height;
-        const ctx = canvas.getContext("2d");
-        ctx.drawImage(img, 0, 0);
-    }; 
-}
-
-  fileInput.addEventListener('change', loadImg);
-  document.addEventListener("DOMContentLoaded", drawCanvas);
-  const dataURL = canvas.toDataURL("image/jpeg");
 
 /*----------------------------------For save picture end--------------------------------*/
 
