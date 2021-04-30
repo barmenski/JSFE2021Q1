bf73e395cf4a9f67810c1ac2a7878622027967f7
Aliaksandr Bondar <1kiloom@gmail.com>
2021-05-01 02:36:10 +0300

    feat: feedback popup work

--- online-zoo/pages/landing/script.js
+++ online-zoo/pages/landing/script.js
@@ -1,14 +1,79 @@
+/*----------------------------------------Left feedback start---------------------------------------*/
+
+const coverElem = document.getElementById("cover");
+const formElem = document.getElementById("form-feedback");
+const closeElem = document.querySelector(".feedback__close");
+const sendButton = document.getElementById("send");
+const feedBackButton = document.querySelector(".testimonials__btn");
+
+const nameField =  document.getElementById("name");
+const emailField =  document.getElementById("email");
+const feedbackField =  document.getElementById("text-feedback");
+
+const validate = () => {
+  if (nameField.validity.valid &&
+    emailField.validity.valid &&
+    feedbackField.validity.valid){
+  sendButton.classList.remove("invalid");
+  sendButton.removeAttribute("disabled");
+    } else {
+      sendButton.classList.add("invalid");
+      sendButton.setAttribute("disabled","");
+    }
+}
+
+feedBackButton.addEventListener("click", ()=>{
+  document.body.classList.add("notScrollable");
+  coverElem.classList.remove("notVisible");
+  formElem.classList.remove("notVisible");
+})
+
+coverElem.addEventListener("click", ()=>{
+  document.body.classList.remove("notScrollable");
+  coverElem.classList.add("notVisible");
+  formElem.classList.add("notVisible");
+})
+
+closeElem.addEventListener("click", ()=>{
+  document.body.classList.remove("notScrollable");
+  coverElem.classList.add("notVisible");
+  formElem.classList.add("notVisible");
+})
+
+sendButton.addEventListener("click", ()=>{
+  document.body.classList.remove("notScrollable");
+  coverElem.classList.add("notVisible");
+  formElem.classList.add("notVisible");
+})
+
+nameField.addEventListener("input", () => {
+  validate();
+})
+
+emailField.addEventListener("input", () => {
+  validate();
+})
+
+feedbackField.addEventListener("input", () => {
+  validate();
+})
+
+/*----------------------------------------Left feedback end---------------------------------------*/
+
+
+/*----------------------------------------Carousel start---------------------------------------*/
+
+
+
 const state = {};
 const carouselList = document.querySelector('.carousel-list');
 const carouselItemsLi = document.querySelectorAll('.carousel-list__item');
-//console.log(carouselItemsLi);
 const carouselItems = document.querySelectorAll('.carousel-item__img');
 const elems = Array.from(carouselItems);
 
 
 carouselList.addEventListener('click', function (event) {
   var newActive = event.target;
-  //console.log(newActive);
   var isItem = newActive.closest('.carousel-item__img');
 
   if (!isItem || newActive.classList.contains('carousel__item_active')) {
@@ -49,3 +114,4 @@ const getPos = function (current, active) {
   return diff;
 }
 
+/*----------------------------------------Carousel end---------------------------------------*/
